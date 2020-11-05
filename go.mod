5739a674da565cb4b12109dadf1c5c694681c9a2
sheetalrprasad <sheetalraniprasad@gmail.com>
2020-11-05 11:08:49 +0530

    adding swagger doc

---
+++ go.mod
@@ -0,0 +1,32 @@
+module github.com/sheetalrprasad/productapi
+
+go 1.15
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
+	github.com/go-openapi/analysis v0.19.11 // indirect
+	github.com/go-openapi/errors v0.19.8 // indirect
+	github.com/go-openapi/runtime v0.19.23 // indirect
+	github.com/go-openapi/spec v0.19.12 // indirect
+	github.com/go-openapi/strfmt v0.19.8 // indirect
+	github.com/go-openapi/validate v0.19.12 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-swagger/go-swagger v0.25.0 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/magiconair/properties v1.8.4 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/spf13/afero v1.4.1 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	go.mongodb.org/mongo-driver v1.4.3 // indirect
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
+	golang.org/x/sys v0.0.0-20201101102859-da207088b7d1 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/tools v0.0.0-20201105001634-bc3cf281b174 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0
+	gopkg.in/ini.v1 v1.62.0 // indirect
+)
---
+++ handlers/products.go
@@ -0,0 +1,102 @@
+// Package classification of Product API
+//
+// Documentation of Product API
+// Schemes: http
+// BasePath: /
+// Version: 1.0.0
+//
+// Consumes:
+// - application/json
+//
+// Produces:
+// - application/json
+// swagger:meta
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+
+	"github.com/sheetalrprasad/productapi/data"
+)
+
+// A list of products returns in the response
+//sawgger:response productResponse
+type productsResponse struct {
+	// All produtcs in the system
+	// in:body
+	Body []data.Product
+}
+
+// Products in a http.Handler
+type Products struct {
+	l *log.Logger
+}
+
+// NewProducts creates a products handler with given logger
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+// GetProducts returns a list of all products in the system
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Failed to marshal json.", http.StatusInternalServerError)
+	}
+}
+
+// AddProducts creates a new product to the system
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handling the POST")
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	data.AddProduct(&prod)
+}
+
+// UpdateProduct modifies an existing product in the system
+func (p *Products) UpdateProduct(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "Cannot Caonvert ID to int", http.StatusBadRequest)
+	}
+	p.l.Println("Handling the PUT ", id)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	err = data.UpdateProduct(prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product data not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "Product data not found", http.StatusInternalServerError)
+		return
+	}
+}
+
+type KeyProduct struct{}
+
+func (p *Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+		err := prod.FromJSON(r.Body)
+		if err != nil {
+			http.Error(rw, "Cannot unmarshal", http.StatusBadRequest)
+			return
+		}
+		err = prod.Validate()
+		if err != nil {
+			http.Error(rw, fmt.Sprintf("Error validating product: %s", err), http.StatusBadRequest)
+			return
+		}
+		p.l.Printf("prod %#v", prod) //shows field + value
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(rw, r)
+	})
+}
